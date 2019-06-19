8a521e48cb556efeb615c6e411aa2738d90a473d
Lukas Braun <mail@lukasbraun.com>
2019-06-19 17:27:35 +0200

    Remove old functions and variables that are no longer required and used. Added comments and docstring.

--- tasks/task.py
+++ tasks/task.py
@@ -1,33 +1,58 @@
 import numpy as np
 
 class Task:
-    def __init__(self):
-        self._training_data = []
-        self._training_labels = []
-        self._validation_data = []
-        self._validation_labels = []
-        self._test_data = []
-        self._test_labels = []
+    """
+    A base class for a training task
     
-    def __iter__(self):
-        return self
+    ...
     
-    def __next__(self):
-        if self.step >= self.max_steps:
-            raise StopIteration
-        self.step += 1
-        return self.inputs[self.step-1], self.outputs[self.step-1]
+    Methods
+    -------
+    training_data(batch_size)
+        iterate over training data and corresponding labels
+    
+    validation_data(batch_size)
+        iterate over validation data and corresponding labels
+    
+    test_data(batch_size)
+        iterate over test data and corresponding labels
+    
+    """
+    def __init__(self):  
+        """Create placeholders for data"""
+        self._training_data = np.array([])
+        self._training_labels = np.array([])
+        self._validation_data = np.array([])
+        self._validation_labels = np.array([])
+        self._test_data = np.array([])
+        self._test_labels = np.array([])
     
     def training_data(self, batch_size):
+        """Iterate over training data and corresponding labels
+        
+        :param batch_size: (int) size of batches
+        :returns: a generator returning batches of training data and corresponding labels
+        """
         return self._get_batch(self._training_data, self._training_labels, batch_size)
     
     def validation_data(self, batch_size):
+        """Iterate over validation data and corresponding labels
+        
+        :param batch_size: (int) size of batches
+        :returns: a generator returning batches of validation data and corresponding labels
+        """
         return self._get_batch(self._validation_data, self._validation_labels, batch_size)
     
     def test_data(self, batch_size):
+        """Iterate over test data and corresponding labels
+        
+        :param batch_size: (int) size of batches
+        :returns: a generator returning batches of test data and corresponding labels
+        """
         return self._get_batch(self._test_data, self._test_labels, batch_size)
     
     def _get_batch(self, data, labels, batch_size):
+        # Shuffle data and return batches of data and labels in a generator
         samples_n = labels.shape[0]
         if batch_size <= 0:
             batch_size = samples_n
